d4af39aca59c62171cb7de6374c581fb6e1959f6
Logan Leger <logan@loganleger.com>
2011-03-08 17:26:01 -0600

    Add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110308232143) do
+ActiveRecord::Schema.define(:version => 20110308232538) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(:version => 20110308232143) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "activated"
+    t.string   "name"
   end
 
 end
