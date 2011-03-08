fa6f4cf8234db504248bfab552b30ac4861ffae6
Logan Leger <logan@loganleger.com>
2011-03-08 17:22:34 -0600

    Add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def activate
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Yeartogo::Application.routes.draw do
+  get "users/show"
+
+  get "users/new"
+
+  get "users/create"
+
+  get "users/destroy"
+
+  get "users/edit"
+
+  get "users/update"
+
+  get "users/activate"
+
 	scope "static" do
 		get "home" => "static#home"
 		get "about" => "static#about"
