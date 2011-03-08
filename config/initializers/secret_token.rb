8a064316bb1d180c81e46edd7ff0cbc389121208
Logan Leger <logan@loganleger.com>
2011-03-08 16:56:23 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yeartogo::Application.config.secret_token = '299d53e426b3d238e912aee5da7539329f8911030fbbd0e81d29417dd466606581b201ec71d6af79f6665e2844fb5de15b72c196c6c5041492f2a6c6ca29b528'
