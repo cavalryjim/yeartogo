e74fc1f9ba9bd4314f3b10d18c1fd696075dc7e0
Logan Leger <logan@loganleger.com>
2011-03-08 20:30:22 -0600

    User register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,17 @@ class UsersController < ApplicationController
   end
 
   def new
+		@user = User.new
   end
 
   def create
+		@user = User.new(params[:user])
+		if @user.save
+			redirect_to login_url, :notice => "Signed up! Please check your email for activation instructions."
+			#Notifications.verification(@person.id).deliver
+		else
+			render "new"
+		end
   end
 
   def destroy
