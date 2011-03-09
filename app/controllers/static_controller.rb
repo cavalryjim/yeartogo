62930f99017d399723c7eb504abd93b538e71af1
Logan Leger <logan@loganleger.com>
2011-03-09 02:35:08 -0600

    Setup variables for home page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,7 @@
 class StaticController < ApplicationController
   def home
+		@completed = Goal.order("updated_at DESC").where("completed = ? AND privacy = ?",true,2)
+		@categories = Category.all(:include => :goals).sort_by { |c| -c.goals.size }
   end
 
   def about
