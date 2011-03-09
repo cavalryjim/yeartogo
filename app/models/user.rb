f9087e787cdee61c648c9e7b0688968a517e6851
Logan Leger <logan@loganleger.com>
2011-03-09 02:31:47 -0600

    Add about to attr_accessible in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :goals
 	
-	attr_accessible :email, :password, :password_confirmation, :avatar, :name, :username, :location
+	attr_accessible :email, :password, :password_confirmation, :avatar, :name, :username, :location, :about
 	attr_accessor :password
 	
 	before_save :encrypt_password
