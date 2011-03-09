12d706579231da8f41e2de8c199e966877082722
Logan Leger <logan@loganleger.com>
2011-03-08 22:57:09 -0600

    Add user_id to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110309044547) do
+ActiveRecord::Schema.define(:version => 20110309045647) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20110309044547) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "goal_id"
+    t.integer  "user_id"
   end
 
   create_table "goals", :force => true do |t|
