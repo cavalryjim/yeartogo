6dfc92301f874437b0bf678657d1b382ae3ed6fe
Logan Leger <logan@loganleger.com>
2011-03-09 23:17:53 -0600

    Unique motivated users list for homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,11 @@ class StaticController < ApplicationController
   def home
 		@completed = Goal.order("updated_at DESC").where("completed = ? AND privacy = ?",true,1)
 		@categories = Category.all(:include => :goals).sort_by { |c| -c.goals.size }
+		motivated_users = @completed.map(&:user).map(&:id).uniq!
+		@motivated = Array.new
+		motivated_users.each do |u|
+			@motivated << User.find(u)
+		end
   end
 
   def about
