5c61be75e4f8fdcbdc34227b65e1b37fddd78476
Logan Leger <logan@loganleger.com>
2011-03-10 02:44:50 -0600

    Limits on landing page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def home
-		@completed = Goal.order("updated_at DESC").where("completed = ? AND privacy = ?",true,1)
+		@completed = Goal.order("updated_at DESC").where("completed = ? AND privacy = ?",true,1).limit(10)
 		@categories = Category.all(:include => :goals).sort_by { |c| -c.goals.size }
 		motivated_users = @completed.map(&:user).map(&:id).uniq!
 		@motivated = Array.new
