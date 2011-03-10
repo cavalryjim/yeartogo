ad9a88138c405e88132ea7e71e11ee7491a1dc1a
Logan Leger <logan@loganleger.com>
2011-03-10 02:38:17 -0600

    Goal seeds from Brian, commented out for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,17 @@
 
 Category.create([{:name => "Financial"}, {:name => "Health"}, {:name => "Education"}, {:name => "Travel"}
 								{:name => "Career"}])
+								
+# Goal.create(:name="Run 50 Marathons",timeline="0 0 0")
+# Goal.create(:name="Lose 100 pounds",timeline="0 0 0")
+# Goal.create(:name="Learn to play the guitar",timeline="0 0 0")
+# Goal.create(:name="Save $1000",timeline="0 0 0")
+# Goal.create(:name="Learn to cook pasta",timeline="0 0 0")
+# Goal.create(:name="Build a website",timeline="0 0 0")
+# Goal.create(:name="Pass the MCAT",timeline="0 0 0")
+# Goal.create(:name="Improve my GPA",timeline="0 0 0")
+# Goal.create(:name="Learn to play the piano",timeline="0 0 0")
+# Goal.create(:name="Improve my batting average",timeline="0 0 0")
+# Goal.create(:name="Get my first patent",timeline="0 0 0")
+# Goal.create(:name="Buy a domain name",timeline="0 0 0")
+# Goal.create(:name="Increase my profit margin",timeline="0 0 0")
