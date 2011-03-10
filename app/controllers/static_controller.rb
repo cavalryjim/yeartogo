f4ce0d259337515db0405b4c4e83419ae5276cb3
Logan Leger <logan@loganleger.com>
2011-03-10 11:29:25 -0600

    Home page updates

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,10 +3,13 @@ class StaticController < ApplicationController
 		@completed = Goal.order("updated_at DESC").where("completed = ? AND privacy = ?",true,1).limit(10)
 		@categories = Category.all(:include => :goals).sort_by { |c| -c.goals.size }
 		motivated_users = @completed.map(&:user).map(&:id).uniq!
-		@motivated = Array.new
-		motivated_users.each do |u|
-			@motivated << User.find(u)
+		unless motivated_users.nil?
+			@motivated = Array.new
+			motivated_users.each do |u|
+				@motivated << User.find(u)
+			end			
 		end
+		@user = User.new
   end
 
   def about
