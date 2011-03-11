012fbdd4b19fd8cd2233f1c10ec867815fc12398
Logan Leger <logan@loganleger.com>
2011-03-10 19:06:29 -0600

    Fix pending requests bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-	helper_method :current_user
+	helper_method :current_user, :pending_requests
 	
 	private
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+	
+	def pending_requests
+		@pending_requests ||= Friendship.find_all_by_friend_id_and_status(current_user.id,0) if current_user
 	end
 end
